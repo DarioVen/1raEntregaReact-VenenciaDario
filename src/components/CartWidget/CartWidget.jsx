fa6e169cc1f58434127ca934ab2a004c29531bd9
DarioVen <darioalev@gmail.com>
2024-11-13 17:55:18 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,21 @@
+import { StarIcon } from "@chakra-ui/icons";
+
+ 
+
+const CartWidget = () => {
+
+  return (
+
+    <div>
+
+      <StarIcon />0
+
+    </div>
+
+  );
+
+};
+
+ 
+
+export default CartWidget;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,31 @@
+import { Flex } from "@chakra-ui/react";
+
+ 
+
+const ItemListContainer = ({ greeting }) => {
+
+  return (
+
+    <Flex
+
+      alignItems={"center"}
+
+      justifyContent={"center"}
+
+      height={"90vh"}
+
+      fontSize={"2rem"}
+
+    >
+
+      {greeting}
+
+    </Flex>
+
+  );
+
+};
+
+ 
+
+export default ItemListContainer;
